71ed25f277e3f9ea2bb8d8fc93b8ff716ec3528a
stud-diniz <stud-diniz@ruc.dk>
2026-05-13 16:20:47 +0200

    changed size of entrance and funnel end to match... still need to calculate angles

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ h = 1           # Radius of search in meter
 m = 80          # Mass in kg
 
 # Speeds
-sl = 2          # Multiplier on the random start speed
+sl = 1          # Multiplier on the random start speed
 v_pref = 1.4    # Preferred speed in m/s (matches paper)
 
 # Particle forces
-strength = 10    # Multiplier on the force between particles
+strength = 20    # Multiplier on the force between particles
 cutoff = 5      # Threshold of proximity
 long_strength = 0.5  # Multiplier on the force between distant particles
 long_cutoff = 10     # Threshold of proximity of distant particles
@@ -21,11 +21,11 @@ k_sph = 1.0    # Gas constant — stiffness of pressure response
 mu = 5.0        # Viscosity — dampens relative motion between neighbors
 
 # Wall forces
-wallf = 1.0     # Walls repellant force
+wallf = 2.0     # Walls repellant force
 
 # Door
-door_x1 = 12.5
-door_x2 = 18.5
+door_x1 = 14.75
+door_x2 = 16.25
 # Door attactive force location
 goal_x = 15.5   # center of door (12.5 to 18.5)
 goal_y = 35.0   # just above the top wall (30.5) to pull them through
