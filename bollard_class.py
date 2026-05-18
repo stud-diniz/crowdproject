2cd89a199988c68d2585b5940c4676facc52d709
stud-diniz <stud-diniz@ruc.dk>
2026-05-18 10:15:22 +0200

    experiment data

--- bollard_class.py
+++ bollard_class.py
@@ -81,6 +81,7 @@ class obstacle:
 
 class bollard_pos():
     centered = [obstacle(15.5, 38.5, rar, splitter_length=5.0)]
-    far = [obstacle(15.5, 35.5, rar, influence=8.0, strength=3.0)]
+    far      = [obstacle(15.5, 35.5, rar, influence=8.0, strength=3.0)]
     hug      = [obstacle(16.5, 37.5, rar)]
+    close    = [obstacle(15.5, 40, rar)]
     void     = []
